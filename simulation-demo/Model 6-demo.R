71a1d67498e79bdd431f34cb0ef73e83f7955b5a
JASA-Authors <lpmeugs@gmail.com>
2024-08-04 10:55:32 -0700

    Update Model 6-demo.R

--- simulation-demo/Model 6-demo.R
+++ simulation-demo/Model 6-demo.R
@@ -11,23 +11,23 @@ set.seed(1234)
 ################
 ### Function ###
 ################
-PCAkmeans=function(X, K, lambda1, lambda2, theta, Sigmatrue){
+Group_Detect=function(X, K, lambda1, lambda2, theta, Sigmatrue){
   # OUR method
   # input:
   # X: data
-  # K: number of groups (must be 4)
-  # theta: judge when to stop
-  # lambda1: parameter for adapted huber regression
+  # K: number of groups
+  # theta: threshold for stop
+  # lambda1: parameter for glmnet
   # lambda2: parameter for glasso regression
   
   # output:
-  # Omega: estimated inversed covariance matrix
+  # Omega: estimated precision matrix
   # error: estimation error
   
   n=nrow(X)
   p=ncol(X)
   
-  # each row of X minuses its mean
+  # centralize each row
   Y=sweep(X,1,rowMeans(X))
   
   # PCA
@@ -37,12 +37,14 @@ PCAkmeans=function(X, K, lambda1, lambda2, theta, Sigmatrue){
   # Kmeans
   clusterinf=kmeans(V, K)$cluster
   
+  # detect orders based on cluster size
   B_count=c()
   for (i in 1:K) {
     B_count=c(B_count,length(which(clusterinf==i)))
   }
   seq1=order(B_count,decreasing = FALSE)
   
+  # reorder sample X
   Bcluster=list()
   C=c()
   for (i in seq1) {
@@ -167,8 +169,19 @@ PCAkmeans=function(X, K, lambda1, lambda2, theta, Sigmatrue){
   return(list(Omega=newOmega, error=error))
 }
 
-BCDnormal=function(X,K,lambda1,lambda2,theta, Sigmatrue){
-  # Let K=p, and this is NO-GROUP method
+Group_Precision=function(X,K,lambda1,lambda2,theta, Sigmatrue){
+  # Let K=P, it becomes NO-GROUP method 
+  # input:
+  # X: data
+  # K: number of groups
+  # theta: judge when to stop
+  # lambda1: parameter for glmnet
+  # lambda2: parameter for glasso regression
+  
+  # output:
+  # Omega: estimated precision matrix
+  # error: estimation error
+  
   n=nrow(X)
   p=ncol(X)
   numblock=K
@@ -300,8 +313,8 @@ Sigmatrue=ar1_cor(p,0.95) # true covariance matrix
 for(rep in 1:replication_time){
   X=as.matrix(MASS::mvrnorm(n=n,mu=rep(0,p),Sigma = Sigmatrue))
   
-  error1=PCAkmeans(X,K,lambda1,lambda2,theta, Sigmatrue)$error
-  error3=BCDnormal(X,p,lambda1,lambda2,theta, Sigmatrue)$error
+  error1=Group_Detect(X,K,lambda1,lambda2,theta, Sigmatrue)$error
+  error3=Group_Precision(X,p,lambda1,lambda2,theta, Sigmatrue)$error
   error5=sqrt(sum((solve(Sigmatrue)-glasso::glasso(cov(X), rho=.1)$wi)^2))
   error6=sqrt(sum((solve(Sigmatrue)-flare::sugm(X,nlambda=1,method = "tiger")$icov[[1]])^2))
   error7=sqrt(sum((solve(Sigmatrue)-flare::sugm(X,nlambda=1,method = "clime")$icov[[1]])^2))
@@ -340,8 +353,8 @@ Sigmatrue=ar1_cor(p,0.95) # true covariance matrix
 for(rep in 1:replication_time){
   X=as.matrix(MASS::mvrnorm(n=n,mu=rep(0,p),Sigma = Sigmatrue))
   
-  error1=PCAkmeans(X,K,lambda1,lambda2,theta, Sigmatrue)$error
-  error3=BCDnormal(X,p,lambda1,lambda2,theta, Sigmatrue)$error
+  error1=Group_Detect(X,K,lambda1,lambda2,theta, Sigmatrue)$error
+  error3=Group_Precision(X,p,lambda1,lambda2,theta, Sigmatrue)$error
   error5=sqrt(sum((solve(Sigmatrue)-glasso::glasso(cov(X), rho=.1)$wi)^2))
   error6=sqrt(sum((solve(Sigmatrue)-flare::sugm(X,nlambda=1,method = "tiger")$icov[[1]])^2))
   error7=sqrt(sum((solve(Sigmatrue)-flare::sugm(X,nlambda=1,method = "clime")$icov[[1]])^2))
@@ -379,8 +392,8 @@ Sigmatrue=ar1_cor(p,0.95) # true covariance matrix
 for(rep in 1:replication_time){
   X=as.matrix(MASS::mvrnorm(n=n,mu=rep(0,p),Sigma = Sigmatrue))
   
-  error1=PCAkmeans(X,K,lambda1,lambda2,theta, Sigmatrue)$error
-  error3=BCDnormal(X,p,lambda1,lambda2,theta, Sigmatrue)$error
+  error1=Group_Detect(X,K,lambda1,lambda2,theta, Sigmatrue)$error
+  error3=Group_Precision(X,p,lambda1,lambda2,theta, Sigmatrue)$error
   error5=sqrt(sum((solve(Sigmatrue)-glasso::glasso(cov(X), rho=.1)$wi)^2))
   error6=sqrt(sum((solve(Sigmatrue)-flare::sugm(X,nlambda=1,method = "tiger")$icov[[1]])^2))
   error7=sqrt(sum((solve(Sigmatrue)-flare::sugm(X,nlambda=1,method = "clime")$icov[[1]])^2))
@@ -418,8 +431,8 @@ Sigmatrue=ar1_cor(p,0.95) # true covariance matrix
 for(rep in 1:replication_time){
   X=as.matrix(MASS::mvrnorm(n=n,mu=rep(0,p),Sigma = Sigmatrue))
   
-  error1=PCAkmeans(X,K,lambda1,lambda2,theta, Sigmatrue)$error
-  error3=BCDnormal(X,p,lambda1,lambda2,theta, Sigmatrue)$error
+  error1=Group_Detect(X,K,lambda1,lambda2,theta, Sigmatrue)$error
+  error3=Group_Precision(X,p,lambda1,lambda2,theta, Sigmatrue)$error
   error4=sqrt(sum((solve(Sigmatrue)-solve(cov(X)))^2))
   error5=sqrt(sum((solve(Sigmatrue)-glasso::glasso(cov(X), rho=.1)$wi)^2))
   error6=sqrt(sum((solve(Sigmatrue)-flare::sugm(X,nlambda=1,method = "tiger")$icov[[1]])^2))
