66a29eb73c82a90aa57e7a6c5ffbb2be4d166a59
JASA-Authors <lpmeugs@gmail.com>
2024-08-04 22:06:52 -0700

    Update Model 5.R

--- simulation-code /Model 5.R
+++ simulation-code /Model 5.R
@@ -7,32 +7,32 @@ library(POET)
 library(foreach)
 library(doParallel)
 
-clnum<-detectCores()-8 # should be 24
+clnum<-24
 cl <- makeCluster(getOption("cl.cores", clnum))
 registerDoParallel(cl)
 
-set.seed(1234)
+
 
 ################
 ### Function ###
 ################
-PCAkmeans=function(X, K, lambda1, lambda2, theta, Sigmatrue){
+Group_Detect=function(X, K, lambda1, lambda2, theta, Sigmatrue){
   # OUR method
   # input:
   # X: data
-  # K: number of groups (must be 4)
-  # theta: judge when to stop
-  # lambda1: parameter for adapted huber regression
+  # K: number of groups
+  # theta: threshold for stop
+  # lambda1: parameter for glmnet
   # lambda2: parameter for glasso regression
   
   # output:
-  # Omega: estimated inversed covariance matrix
+  # Omega: estimated precision matrix
   # error: estimation error
   
   n=nrow(X)
   p=ncol(X)
   
-  # each row of X minuses its mean
+  # centralize each row
   Y=sweep(X,1,rowMeans(X))
   
   # PCA
@@ -42,12 +42,14 @@ PCAkmeans=function(X, K, lambda1, lambda2, theta, Sigmatrue){
   # Kmeans
   clusterinf=kmeans(V, K)$cluster
   
+  # detect orders based on cluster size
   B_count=c()
   for (i in 1:K) {
     B_count=c(B_count,length(which(clusterinf==i)))
   }
   seq1=order(B_count,decreasing = FALSE)
   
+  # reorder sample X
   Bcluster=list()
   C=c()
   for (i in seq1) {
@@ -172,9 +174,20 @@ PCAkmeans=function(X, K, lambda1, lambda2, theta, Sigmatrue){
   return(list(Omega=newOmega, error=error))
 }
 
-BCDnormal=function(X,K,lambda1,lambda2,theta, Sigmatrue){
+Group_Precision=function(X,K,lambda1,lambda2,theta, Sigmatrue){
   # ORACLE method
-  # when K=p, it is NO-GROUP method
+  # when K=P, it becomes NO-GROUP method 
+  # input:
+  # X: data
+  # K: number of groups
+  # theta: judge when to stop
+  # lambda1: parameter for glmnet
+  # lambda2: parameter for glasso regression
+  
+  # output:
+  # Omega: estimated precision matrix
+  # error: estimation error
+  
   n=nrow(X)
   p=ncol(X)
   numblock=K
@@ -306,11 +319,12 @@ for (i in 1:(p-1)) {
 }
 
 compare_par=function(i, Sigmatrue, n, p){
+  set.seed(i)
   X=as.matrix(MASS::mvrnorm(n=n,mu=rep(0,p),Sigma = Sigmatrue))
   
-  error1=PCAkmeans(X,K,lambda1,lambda2,theta, Sigmatrue)$error
-  error2=BCDnormal(X,K,lambda1,lambda2,theta, Sigmatrue)$error
-  error3=BCDnormal(X,p,lambda1,lambda2,theta, Sigmatrue)$error
+  error1=Group_Detect(X,K,lambda1,lambda2,theta, Sigmatrue)$error
+  error2=Group_Precision(X,K,lambda1,lambda2,theta, Sigmatrue)$error
+  error3=Group_Precision(X,p,lambda1,lambda2,theta, Sigmatrue)$error
   error5=sqrt(sum((solve(Sigmatrue)-glasso::glasso(cov(X), rho=.1)$wi)^2))
   error6=sqrt(sum((solve(Sigmatrue)-flare::sugm(X,nlambda=1,method = "tiger")$icov[[1]])^2))
   error7=sqrt(sum((solve(Sigmatrue)-flare::sugm(X,nlambda=1,method = "clime")$icov[[1]])^2))
@@ -321,6 +335,9 @@ compare_par=function(i, Sigmatrue, n, p){
 }
 
 x <- foreach(i=1:200,.combine='rbind') %dopar% compare_par(i, Sigmatrue5, n, p)
+colnames(x)=c("OUR","ORACLE","NO-GROUP","G-LASSO","TIGER","CLIME","POET")
+x=rbind(x,apply(x,2,mean))
+x=rbind(x,apply(x,2,sd))
 
 write.csv(x,file=paste0("model5_", n,"_", p, ".csv"),quote=F,row.names = F)
 
@@ -348,11 +365,12 @@ for (i in 1:(p-1)) {
 }
 
 compare_par=function(i, Sigmatrue, n, p){
+  set.seed(i)
   X=as.matrix(MASS::mvrnorm(n=n,mu=rep(0,p),Sigma = Sigmatrue))
   
-  error1=PCAkmeans(X,K,lambda1,lambda2,theta, Sigmatrue)$error
-  error2=BCDnormal(X,K,lambda1,lambda2,theta, Sigmatrue)$error
-  error3=BCDnormal(X,p,lambda1,lambda2,theta, Sigmatrue)$error
+  error1=Group_Detect(X,K,lambda1,lambda2,theta, Sigmatrue)$error
+  error2=Group_Precision(X,K,lambda1,lambda2,theta, Sigmatrue)$error
+  error3=Group_Precision(X,p,lambda1,lambda2,theta, Sigmatrue)$error
   error5=sqrt(sum((solve(Sigmatrue)-glasso::glasso(cov(X), rho=.1)$wi)^2))
   error6=sqrt(sum((solve(Sigmatrue)-flare::sugm(X,nlambda=1,method = "tiger")$icov[[1]])^2))
   error7=sqrt(sum((solve(Sigmatrue)-flare::sugm(X,nlambda=1,method = "clime")$icov[[1]])^2))
@@ -363,6 +381,9 @@ compare_par=function(i, Sigmatrue, n, p){
 }
 
 x <- foreach(i=1:200,.combine='rbind') %dopar% compare_par(i, Sigmatrue5, n, p)
+colnames(x)=c("OUR","ORACLE","NO-GROUP","G-LASSO","TIGER","CLIME","POET")
+x=rbind(x,apply(x,2,mean))
+x=rbind(x,apply(x,2,sd))
 
 write.csv(x,file=paste0("model5_", n,"_", p, ".csv"),quote=F,row.names = F)
 
@@ -390,11 +411,12 @@ for (i in 1:(p-1)) {
 }
 
 compare_par=function(i, Sigmatrue, n, p){
+  set.seed(i)
   X=as.matrix(MASS::mvrnorm(n=n,mu=rep(0,p),Sigma = Sigmatrue))
   
-  error1=PCAkmeans(X,K,lambda1,lambda2,theta, Sigmatrue)$error
-  error2=BCDnormal(X,K,lambda1,lambda2,theta, Sigmatrue)$error
-  error3=BCDnormal(X,p,lambda1,lambda2,theta, Sigmatrue)$error
+  error1=Group_Detect(X,K,lambda1,lambda2,theta, Sigmatrue)$error
+  error2=Group_Precision(X,K,lambda1,lambda2,theta, Sigmatrue)$error
+  error3=Group_Precision(X,p,lambda1,lambda2,theta, Sigmatrue)$error
   error4=sqrt(sum((solve(Sigmatrue)-solve(cov(X)))^2))
   error5=sqrt(sum((solve(Sigmatrue)-glasso::glasso(cov(X), rho=.1)$wi)^2))
   error6=sqrt(sum((solve(Sigmatrue)-flare::sugm(X,nlambda=1,method = "tiger")$icov[[1]])^2))
@@ -406,6 +428,9 @@ compare_par=function(i, Sigmatrue, n, p){
 }
 
 x <- foreach(i=1:200,.combine='rbind') %dopar% compare_par(i, Sigmatrue5, n, p)
+colnames(x)=c("OUR","ORACLE","NO-GROUP","SAMPLE","G-LASSO","TIGER","CLIME","POET")
+x=rbind(x,apply(x,2,mean))
+x=rbind(x,apply(x,2,sd))
 
 write.csv(x,file=paste0("model5_", n,"_", p, ".csv"),quote=F,row.names = F)
 
@@ -433,11 +458,12 @@ for (i in 1:(p-1)) {
 }
 
 compare_par=function(i, Sigmatrue, n, p){
+  set.seed(i)
   X=as.matrix(MASS::mvrnorm(n=n,mu=rep(0,p),Sigma = Sigmatrue))
   
-  error1=PCAkmeans(X,K,lambda1,lambda2,theta, Sigmatrue)$error
-  error2=BCDnormal(X,K,lambda1,lambda2,theta, Sigmatrue)$error
-  error3=BCDnormal(X,p,lambda1,lambda2,theta, Sigmatrue)$error
+  error1=Group_Detect(X,K,lambda1,lambda2,theta, Sigmatrue)$error
+  error2=Group_Precision(X,K,lambda1,lambda2,theta, Sigmatrue)$error
+  error3=Group_Precision(X,p,lambda1,lambda2,theta, Sigmatrue)$error
   error4=sqrt(sum((solve(Sigmatrue)-solve(cov(X)))^2))
   error5=sqrt(sum((solve(Sigmatrue)-glasso::glasso(cov(X), rho=.1)$wi)^2))
   error6=sqrt(sum((solve(Sigmatrue)-flare::sugm(X,nlambda=1,method = "tiger")$icov[[1]])^2))
@@ -449,6 +475,9 @@ compare_par=function(i, Sigmatrue, n, p){
 }
 
 x <- foreach(i=1:200,.combine='rbind') %dopar% compare_par(i, Sigmatrue5, n, p)
+colnames(x)=c("OUR","ORACLE","NO-GROUP","SAMPLE","G-LASSO","TIGER","CLIME","POET")
+x=rbind(x,apply(x,2,mean))
+x=rbind(x,apply(x,2,sd))
 
 write.csv(x,file=paste0("model5_", n,"_", p, ".csv"),quote=F,row.names = F)
 
@@ -477,11 +506,12 @@ for (i in 1:(p-1)) {
 }
 
 compare_par=function(i, Sigmatrue, n, p){
+  set.seed(i)
   X=as.matrix(MASS::mvrnorm(n=n,mu=rep(0,p),Sigma = Sigmatrue))
   
-  error1=PCAkmeans(X,K,lambda1,lambda2,theta, Sigmatrue)$error
-  error2=BCDnormal(X,K,lambda1,lambda2,theta, Sigmatrue)$error
-  error3=BCDnormal(X,p,lambda1,lambda2,theta, Sigmatrue)$error
+  error1=Group_Detect(X,K,lambda1,lambda2,theta, Sigmatrue)$error
+  error2=Group_Precision(X,K,lambda1,lambda2,theta, Sigmatrue)$error
+  error3=Group_Precision(X,p,lambda1,lambda2,theta, Sigmatrue)$error
   error5=sqrt(sum((solve(Sigmatrue)-glasso::glasso(cov(X), rho=.1)$wi)^2))
   error6=sqrt(sum((solve(Sigmatrue)-flare::sugm(X,nlambda=1,method = "tiger")$icov[[1]])^2))
   error7=sqrt(sum((solve(Sigmatrue)-flare::sugm(X,nlambda=1,method = "clime")$icov[[1]])^2))
@@ -492,6 +522,9 @@ compare_par=function(i, Sigmatrue, n, p){
 }
 
 x <- foreach(i=1:200,.combine='rbind') %dopar% compare_par(i, Sigmatrue5, n, p)
+colnames(x)=c("OUR","ORACLE","NO-GROUP","G-LASSO","TIGER","CLIME","POET")
+x=rbind(x,apply(x,2,mean))
+x=rbind(x,apply(x,2,sd))
 
 write.csv(x,file=paste0("model5_", n,"_", p, ".csv"),quote=F,row.names = F)
 
